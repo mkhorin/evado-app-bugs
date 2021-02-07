3e71d2e14f312c2ee2fd31dbedaa1dd8dabcc6db
mkhorin <maksimovichu@gmail.com>
2021-02-07 13:56:30 +1000

    Move sidebar menu to configuration

--- config/default.js
+++ config/default.js
@@ -74,6 +74,7 @@ module.exports = {
     utilities: require('./default-utilities'),
     eventHandlers: require('./default-eventHandlers'),
     listeners: require('./default-listeners'),
+    sideMenu: require('./default-sideMenu'),
     params: {
         'enablePasswordChange': true,
         'enablePasswordReset': false,
