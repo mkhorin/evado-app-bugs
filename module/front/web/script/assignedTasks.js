f9a2648c67bcbc39efdb2b080ceb7ab70644d934
mkhorin <maksimovichu@gmail.com>
2023-12-25 09:14:13 +1000

    Update 5.7.1

--- module/front/web/script/assignedTasks.js
+++ module/front/web/script/assignedTasks.js
@@ -29,13 +29,13 @@ Vue.component('assigned-tasks', {
             await this.load(0);
         },
         async load (page) {
+            const {pageSize} = this;
             const data = await this.fetchJson('list', {
                 class: 'task',
                 view: 'listAssignedMe',
-                length: this.pageSize,
-                start: page * this.pageSize
+                length: pageSize,
+                start: page * pageSize
             });
-            const pageSize = this.pageSize;
             this.$emit('load', {...data, pageSize, page});
         },
         onLoad ({items}) {
